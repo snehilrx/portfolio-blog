2790b3b8394f29566ae5680c645c87e24101e2e7
deicidium0 <deicidium@proton.me>
2023-04-06 21:03:29 +0530

    Front Page Design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,58 @@
 import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { Text, Flex, Container, MediaQuery, AppShell } from '@mantine/core';
+import { SideRibbon } from '../components/SideRibbon/SideRibbon';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithubAlt } from '@fortawesome/free-brands-svg-icons'
+
+import { createStyles } from '@mantine/core';
+import { MaterialNavbar } from '../components/Navbar/MaterialNavbar';
+
+
+const Side = () => {
+  const { classes } = createStyles((theme) => ({
+    email: {
+      color: theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.green[6],
+      fontFamily: "mono",
+      writingMode: 'vertical-rl',
+      height: '60%',
+    },
+    iconContainer: {
+      height: '60%',
+      color: theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.green[6],
+      width: '100%',
+      alignItems: 'center',
+      justifyContent: 'end'
+    }
+  }))();
+  return <MediaQuery smallerThan="sm" styles={{ visibility: "hidden" }}>
+    <Container>
+      <SideRibbon position="bottom-left">
+        <Flex className={classes.iconContainer}
+          gap="xl"
+          justify="flex-center"
+          align="flex-center"
+          direction="column"
+          wrap="nowrap">
+          <FontAwesomeIcon icon={faGithubAlt} />
+          <FontAwesomeIcon icon={faGithubAlt} />
+          <FontAwesomeIcon icon={faGithubAlt} />
+        </Flex>
+      </SideRibbon>
+      <SideRibbon position="top-right">
+        <Text className={classes.email}>snehil101@gmail.com</Text>
+      </SideRibbon>
+    </Container>
+  </MediaQuery>
+}
 
 export default function HomePage() {
   return (
     <>
-      <Welcome />
-      <ColorSchemeToggle />
+      <Side />
+      <Container>
+        <MaterialNavbar />
+        <Welcome />
+      </Container>
     </>
   );
 }

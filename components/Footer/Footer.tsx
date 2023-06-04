c9231de65db62fa3f4b17086fb38f46b5bda9bec
Snehil <snehil.shrivastava@gonuclei.com>
2023-06-04 06:55:36 +0530

    update projects

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import { Text } from "@mantine/core";
 
 export function Footer() {
-    return <Text align="center" sx={{fontFamily: 'fira'}} mt="xl">v1.0.0</Text>
+    return <Text align="center" sx={{fontFamily: 'fira'}} mt="xl">v1.0.1</Text>
 }
